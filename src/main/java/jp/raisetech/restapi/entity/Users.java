849ac20d900df377d56fe519041f8e2e52dda6b9
takahiro-aoki494 <arikatuki@gmail.com>
2023-02-14 23:22:52 +0900

    Lombokの設定、Usersでコンストラクタ、ゲッターをLombokで生成するように変更

--- src/main/java/jp/raisetech/restapi/entity/Users.java
+++ src/main/java/jp/raisetech/restapi/entity/Users.java
@@ -1,31 +1,13 @@
 package jp.raisetech.restapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Users {
     private int id;
     private String name;
     private String birthdate;
     private int pin;
-
-    public Users(int id, String name, String birthdate, int pin) {
-        this.id = id;
-        this.name = name;
-        this.birthdate = birthdate;
-        this.pin = pin;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
-
-    public int getPin() {
-        return pin;
-    }
 }
